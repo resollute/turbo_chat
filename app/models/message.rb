12e7bdef371dfacd56537fef09b1d46767bd66b9
resollute <vteleleiko@gmail.com>
2022-06-02 10:46:06 +0300

    Add like model

---
+++ app/models/message.rb
@@ -0,0 +1,8 @@
+class Message < ApplicationRecord
+  belongs_to :user
+  belongs_to :room
+  has_many :likes, dependent: :destroy
+
+  scope :sorted, -> { order(:id) }
+  validates :body, presence: true
+end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,6 @@
 class Room < ApplicationRecord
   belongs_to :user
+  has_many :messages, -> { sorted }, dependent: :destroy
+
   before_create { self.title = SecureRandom.hex(3) } # for slugs
 end
